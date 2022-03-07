5a98113304c76f54c47327c8d0da9536d7714792
sadnansami <sami2841.shah@gmail.com>
2022-03-07 03:43:30 +0000

    Added Hero Page

--- src/routes/index.svelte
+++ src/routes/index.svelte
@@ -1,7 +1,33 @@
 <script>
-    import "../app.css"
+	import "../app.css"
 </script>
 
 <main>
-    Hello I'm under da water
+	<div class="relative h-screen w-screen bg-primary">
+		<div class="h-fit w-full">
+			<div class="flex flex-row justify-between py-3">
+				<div class="invisible"></div>
+				<div class="text-secondary text-3xl text-center leading-[1.6rem]">SUNNAH<br><span class="font-calligraphy tracking-[0.30rem] text-center">Islamia</span></div>
+				<div>H</div>
+			</div>
+			<div class="h-12 flex flex-row justify-around items-center mx-72">
+				<div class="text-xl text-neutral-500">MEN</div>
+				<div class="text-xl text-neutral-500">WOMEN</div>
+				<div class="text-xl text-neutral-500">CHILDREN</div>
+				<div class="text-xl text-neutral-500">WORSHIP</div>
+				<div class="text-xl text-neutral-500">PERFUME</div>
+				<div class="text-xl text-neutral-500">HOME</div>
+			</div>
+		</div>
+		<span class="absolute top-1/2 -translate-y-1/2 text-8xl leading-[1.2] text-secondary">
+			<span class="pl-12">ELEGANT</span><br>
+			<span class="pl-12">SAUDI-STYLE</span><br>
+			<div class=" pl-12 w-[29.5rem] bg-secondary text-primary">
+				JUBBAHS
+			</div>
+		</span>
+		<span class="hidden lg:block">hello</span>
+		<img src="./images/heroImage.jpg" alt="Hero" class="z-10 absolute right-12 top-1/2 -translate-y-1/2 border-[6px] border-secondary h-[26rem] w-[26rem] shadow-xl shadow-black/25">
+		<div class="absolute right-28 top-1/2 -translate-y-1/3 h-[26rem] w-[26rem] bg-secondary shadow-xl shadow-secondary/25"></div>
+	</div>
 </main>
--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,7 +1,19 @@
 module.exports = {
   content: ['./src/**/*.{html,js,svelte,ts}'],
   theme: {
-    extend: {}
+    screens: {
+      'lg': '1200px',
+    },
+    extend: {
+      fontFamily: {
+        'sans': ['Cormorant Garamond'],
+        'calligraphy': ['Khodijah']
+      },
+      colors: {
+				primary: '#ffffeb',
+				secondary: '#0a5948',
+			}
+    }
   },
   plugins: []
 };
